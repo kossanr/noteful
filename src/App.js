fa54067422f48467ffe267784e7bdbdc7ee30ec8
kossanr <58185702+kossanr@users.noreply.github.com>
2020-12-07 12:38:22 -0600

    implemented delete button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React from "react";
-import data from "./data";
+// import data from "./data";
 import Main from "./Main";
 import Folder from "./Folder";
 import Note from "./Note";
 import Context from "./Context";
 import { Route, Link } from "react-router-dom";
 import "./App.css";
+import config from "./config";
 
 export default class App extends React.Component {
   state = {
-    ...data,
-    addFolder: (folder) =>
-      this.setState({ folders: [...this.state.folders, folder] }),
-    addNote: (note) => this.setState({ notes: [...this.state.notes, note] }),
+    // ...data,
+    notes: [],
+    folders: [],
+    handleDeleteNote: (noteId) => {
+      this.setState({
+        notes: this.state.notes.filter((note) => note.id !== noteId),
+      });
+    },
   };
 
-  // componentDidMount()
+  // addFolder: (folder) =>
+  //   this.setState({ folders: [...this.state.folders, folder] }),
+
+  // addNote: (note) => this.setState({ notes: [...this.state.notes, note] }),
+
+  componentDidMount() {
+    //Promise.all() runs several asynchronous operations in parallel
+    Promise.all([
+      fetch(`${config.API_ENDPOINT}/notes`),
+      fetch(`${config.API_ENDPOINT}/folders`),
+    ])
+      .then(([notesRes, foldersRes]) => {
+        if (!notesRes.ok) return notesRes.json().then((e) => Promise.reject(e));
+        if (!foldersRes.ok)
+          return foldersRes.json().then((e) => Promise.reject(e));
+
+        return Promise.all([notesRes.json(), foldersRes.json()]);
+      })
+      .then(([notes, folders]) => {
+        this.setState({ notes, folders });
+      })
+      .catch((error) => {
+        console.error({ error });
+      });
+  }
+
+  //store responses in thisSetStat
+  //this.setState({})
 
   render() {
     return (
--- src/Note.js
+++ src/Note.js
@@ -1,22 +1,67 @@
-import React, { Component } from "react";
+import React from "react"; //{ Component, useCallback }
 import Context from "./Context";
+import config from "./config";
+import { Redirect } from "react-router-dom";
 
 export default class Note extends React.Component {
   static contextType = Context;
+  //write fetch logic for DELETE request
+  //use a callback context value to update the state
+
+  handleClickDelete = (e) => {
+    e.preventDefault();
+    const { noteId = "" } = this.props.match.params || "";
+    // params exists, noteId exists inside of params
+    // params exists, but noteId doesn't
+    // params doesn't even exist
+    //value of noteId
+    //if delete is successful, redirect to the / path
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          //get the error msg from response
+          return res.json().then((error) => {
+            throw error;
+          });
+        }
+        return res.json();
+      })
+      .then((data) => {
+        // remove it from state
+        this.context.handleDeleteNote(noteId);
+        this.props.history.push("/");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   render() {
     const note =
       this.context.notes.find((n) => n.id === this.props.match.params.noteId) ||
       {};
-    return (
+
+    return note.name ? (
       <section className="Note">
         <div className="Note_title">
           <h2>{note.name}</h2>
           <p>{note.content}</p>
         </div>
-        <button className="Note__delete" type="button">
+        <button
+          onClick={this.handleClickDelete}
+          className="Note__delete"
+          type="button"
+        >
           delete
         </button>
       </section>
+    ) : (
+      <Redirect to="/" />
     );
   }
 }
